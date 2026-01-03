75480f43bac8d2e951cce51592e983df57787117
dhruvcodes85 <dhruvedu8@gmail.com>
2026-01-03 12:46:18 +0530

    Time: 3 ms (99.1%), Space: 18.1 MB (49.48%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,11 +1,9 @@
 import re
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        str = re.sub(r'[^a-zA-Z0-9]', '', s)
-        rev_str = str[::-1]
-        if str.lower() == rev_str.lower():
-            return True
-        else:
-            return False
+        string = ''.join(char for char in s if char.isalnum()).lower()
+        rev_str = string[::-1]
+        return string == rev_str
+
 sol = Solution()
 print(sol.isPalindrome("A man, a plan, a canal: Panama"))
